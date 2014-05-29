84a5ab1762e0b92ce223a61c1c8d468fa1f7ef57
root <root@rakesh-Notebook-PC.(none)>
2014-05-29 18:05:00 +0530

    news.php added

--- news.php
+++ news.php
@@ -1,45 +1,49 @@
+
+<?php require($DOCUMENT_ROOT . "template.php"); ?>
 <html>
-	<body>
-		<form action="#" method="post">
+
+  <head>
+   <script>
+     function hide()
+	{
+		 abc=document.getElementById("event");
+		 abc.style.display='none';
+		 
+	}
+	function func(a)
+	{
+             if(a=="game")
+              {
+	         abc=document.getElementById("event");
+                 abc.style.display='block';
+	      }
+             else{
+                 abc=document.getElementById("event");
+                 abc.style.display='none';
+              }
+	}
+    </script>     
+  </head>
+	<body onload="hide()"> 
+           <div id="news_upload">
+		<form action="newsenter.php" method="post">
 			
-			Blurb:<textarea rows="2" columns="10" name="blurb"></textarea>
-			Topic:<select name="topic"><option value="administration">Administration</option><option value="athletics">Athletics</option><option value="badminton">Badminton</option><option value="cricket">Cricket</option><option value="football">Football</option><option value="hockey">Hockey</option><option value="tabletennis">Table Tennis</option><option value="tennis">Tennis</option></select>
-			News:<textarea rows="10" columns="20" name="news"></textarea>
+			<p>Blurb:<textarea rows="2" columns="10" name="blurb"></textarea></p><br><br>
+			<p>Topic:<select name="topic"><option value="administration">Administration</option><option value="athletics">Athletics</option><option value="badminton">Badminton</option><option value="cricket">Cricket</option><option value="football">Football</option><option value="hockey">Hockey</option><option value="tabletennis">Table Tennis</option><option value="tennis">Tennis</option></select></p><br>
+			<p>News:<br><textarea rows="10" columns="20" name="news"></textarea></p><br>
+                       <p> Tag:<select name="sel"  onclick><option value="0" onclick="func('0')">choose tag</option><option value="game" onclick="func('game')">Upcoming sport meet</option><option value="general_info" onclick="func('general_info')">General info</option></select><br>
+                      <div id="event">
+                        <p>Enter the event name:<input type="text" name="event_name"></p><br>
+                        <p>Enter the event date:<input type="date" name="event_date"></p><br>
+                        <p><select name="event_type" ><option value="team" >Team event</option><option value="individual" >Individual</option><option type="both" >Both</option></select></p><br>
+                      </div>
 			<input type="submit" value="Publish" name="submit">
 		</form>
+            </div>
+
 	</body>
+
 </html>
 
-<?php
+<?php require($DOCUMENT_ROOT . "template2.php"); ?>
 
-	session_start();
-        $submit=$_POST['submit'];
-	if(isset($submit))
-	{
-	// $date=$_POST['date'];    
-	 $blurb=$_POST['blurb'];
-	 $topic=$_POST['topic'];
-	 $news=$_POST['news'];
-          $date=date("Y-m-d");
-    echo $date;  
-         
-         echo "hiji";
-	 
-	 require_once "/var/www/PEDS/player/dbconnect.php";
-	 $id=mysql_query("select * from news ");
-	 
-         if($id)
-	   {
-	    while($row=mysql_fetch_array($id))
-	    $id_val=$row['news_id'];
-	   }
-	 else
-	  $id_val=0;
-         $id_val++;
-         mysql_query("insert into news(news_id,post_date,blurb,game,news) values('$id_val','$date','$blurb','$topic','$news') ") or die(mysql_error());
-         	 
-         echo "ur news has been posted";
-         echo "<a href='news.php'>"."publish more"."</a>";
-         }
-         mysql_close($con);
-?>
