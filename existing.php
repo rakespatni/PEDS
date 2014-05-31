0bd131ddd1d67c4cd1080ff1f4badd39981b1214
root <root@rakesh-Notebook-PC.(none)>
2014-05-31 19:05:54 +0530

    in order registration

--- existing.php
+++ existing.php
@@ -5,7 +5,7 @@ $_SESSION['event_type']=$event_type=$_GET['event_type'];
 $_SESSION['event_id']=$_GET['event_id'];
 $_SESSION['event_date']=$event_date=$_GET['event_date'];
 $_SESSION['male_female']=$_GET['male_female'];
-$_SESSION['game_name']=$_GET['game_name'];
+$_SESSION['game_name']=$game_name=$_GET['game_name'];
 
 echo"<html>
     <head><script>
@@ -51,7 +51,7 @@ if(isset($submit))
 		 $check=0;
 		 while($row=mysql_fetch_array($post))
 		  {
-			   if(($row['team_captain_contact']==$phone)&&($game_name==$row['game_name'])
+			   if(($row['team_captain_contact']==$team_captain_contact)&&($game_name==$row['game_name'])
                                &&(($event_type==$row['indi_or_team'])||($event_type=="Both")))
 	                     	{
                                         $team_id=$row['team_id'];
